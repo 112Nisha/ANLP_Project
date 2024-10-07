33e8fd7e7bae796d10930fa6321eedce53cab1cd
112Nisha <127508325+112Nisha@users.noreply.github.com>
2024-10-07 17:21:22 +0530

    Delete textrank.py

--- textrank.py
+++
@@ -1,9 +0,0 @@
-import spacy
-
-nlp = spacy.load("en_core_web_sm") # Load spaCy model
-nlp.add_pipe("textrank") # Add TextRank to the pipeline
-text = "Your text here"
-doc = nlp(text)
-# Access top phrases using doc._.phrases[:n] where n is the desired number
-for phrase in doc._.phrases[:10]:
-  print(phrase.text)
