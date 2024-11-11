f2d5066b2f50804f96b1416c624119ea8dab0d7b
DELL <bhavani.chalasani@students.iiit.ac.in>
2024-11-11 22:17:53 +0530

    added files

--- evaluate.py
+++ evaluate.py
@@ -2,8 +2,12 @@ import torch
 from params import THRESHOLD
 from transformers import BertTokenizer
 # from part_2 import dataloader_helper
-from stanfordnlp.server import CoreNLPClient
 from discourse_aware_story_gen import predict_discourse_marker
+import stanza
+import logging
+logging.getLogger("stanza").setLevel(logging.ERROR) 
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 BATCH_SIZE = 8
 
@@ -29,6 +33,15 @@ def calculate_distinct_1_2(generated_text):
     
     return distinct_1, distinct_2
 
+def average_mentions_per_coref_chain(story,nlp):
+    doc = nlp(story)
+    coref_chains = doc.coref
+    if not coref_chains:
+        return 0
+    total_mentions = sum(len(chain.mentions) for chain in coref_chains)
+    avg_mentions = total_mentions / len(coref_chains)
+    return avg_mentions
+
 # Pass words
 # Run this command - java -mx8g -cp "/home/dell/github/ANLP_Project/stanford-corenlp-4.5.7/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 60000
 # Make sure java is installed
@@ -36,28 +49,28 @@ def calculate_distinct_1_2(generated_text):
 # pip install stanfordnlp
 # constraint on number of letters, goes to 500 as of now, might have to consider individual sentences and not
 # the entire output batch
-def calculate_coreference_chains(generated_text):
-    with CoreNLPClient(timeout=60000, verbose=True) as client:
-        annotations = client.annotate(generated_text, output_format='json', annotators='tokenize,ssplit,pos,lemma,ner,parse,coref')
-        corefs = annotations['corefs']
-        chains = {}
-        for coref_id, coref_info in corefs.items():
-            if isinstance(coref_info, list):
-                chains[coref_id] = [mention['text'] for mention in coref_info]
+# def calculate_coreference_chains(generated_text):
+#     with CoreNLPClient(timeout=60000, verbose=True) as client:
+#         annotations = client.annotate(generated_text, output_format='json', annotators='tokenize,ssplit,pos,lemma,ner,parse,coref')
+#         corefs = annotations['corefs']
+#         chains = {}
+#         for coref_id, coref_info in corefs.items():
+#             if isinstance(coref_info, list):
+#                 chains[coref_id] = [mention['text'] for mention in coref_info]
         
-        coherence_score = average_coreference_chain_length(chains)
-        return coherence_score
+#         coherence_score = average_coreference_chain_length(chains)
+#         return coherence_score
 
-def average_coreference_chain_length(coreference_chains):
-    total_length = 0
-    total_chains = len(coreference_chains)
-    for chain in coreference_chains.values():
-        total_length += len(chain)
-        if total_chains > 0:
-            average_length = total_length / total_chains
-        else:
-            average_length = 0
-    return average_length
+# def average_coreference_chain_length(coreference_chains):
+#     total_length = 0
+#     total_chains = len(coreference_chains)
+#     for chain in coreference_chains.values():
+#         total_length += len(chain)
+#         if total_chains > 0:
+#             average_length = total_length / total_chains
+#         else:
+#             average_length = 0
+#     return average_length
 
 def coherence_score(generated_text):
     golden_bert = torch.load("bert_model.pth")
@@ -73,7 +86,28 @@ def coherence_score(generated_text):
     return num_unknown / total
 
 def main():
-    pass
+    story = '''
+There was once a pleased elephant who generally harassed smaller animals.
+He would go to the ant colony and shower water on the ants.
+The ants, with their size, could just cry.
+The elephant laughed and threatened the ants that he would kill them.
+The ants had enough and chose to show the elephant a lesson.
+They went straight into the elephant's trunk and started messing with him.
+The elephant started crying in pain. He understood his mistake and apologised to the ants
+and every one of the animals he had harassed.
+There was once a pleased elephant who generally harassed smaller animals.
+He would go to the ant colony and shower water on the ants.
+The ants, with their size, could just cry.
+The elephant laughed and threatened the ants that he would kill them.
+The ants had enough and chose to show the elephant a lesson.
+They went straight into the elephant's trunk and started messing with him.
+The elephant started crying in pain. He understood his mistake and apologised to the ants
+and every one of the animals he had harassed.
+'''
+    nlp = stanza.Pipeline('en', processors='tokenize,mwt,pos,lemma,depparse,coref')
+    metric_val = average_mentions_per_coref_chain(story,nlp)
+    print(f"Average Coreference Val: {metric_val}")
+
 
 if __name__ == "__main__":
     main()
